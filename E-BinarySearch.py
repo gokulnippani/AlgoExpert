b06decd050865d0aa9902bdd21f7c3020a487c9c
Gokul Chagalamarri Nippani <gokulnippani@gmail.com>
2019-08-14 10:47:02 -0500

    Binary search iteratively - Solution 2

--- E-BinarySearch.py
+++ E-BinarySearch.py
@@ -1,3 +1,5 @@
+# Solution 1 is recursively. Because we have call stack
+# Solution 2 is iterative.
 def binarySearch(array, target):
     if not array:
         return -1
@@ -5,16 +7,15 @@ def binarySearch(array, target):
 
 
 def binarySearchHelper(low, high, array, target):
-    if low > high:
-        return -1
-    # Type conversion. Change float to int.
-    mid = int((low + high) / 2)
-    if array[mid] == target:
-        return mid
-    elif array[mid] < target:
-        return binarySearchHelper(mid + 1, high, array, target)
-    else:
-        return binarySearchHelper(low, mid - 1, array, target)
-
+    while low <= high:
+        # Type conversion. Change float to int.
+        mid = int((low + high) / 2)
+        if array[mid] == target:
+            return mid
+        elif array[mid] < target:
+            low = mid+1
+        else:
+            high = mid-1
+    return -1
 
 print(binarySearch([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 33))
